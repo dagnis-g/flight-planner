9dff3a83e2ff1bcee597ff49c12366480b95cc75
Dagnis Gulbis <dagnisgulbis1@gmail.com>
2022-05-13 18:37:20 +0300

    repository test

---
+++ src/test/java/io/codelex/flightplanner/FlightRepositoryTest.java
@@ -0,0 +1,37 @@
+package io.codelex.flightplanner;
+
+import io.codelex.flightplanner.controllers.AdminController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class FlightRepositoryTest {
+
+    @Autowired
+    AdminController adminController;
+
+    @Test
+    void addFlight() {
+        Airport from = new Airport("Latvia", "Riga", "RIX");
+        Airport to = new Airport("Estonia", "Tallin", "EST");
+        String departureTime = "2022-04-02 08:30";
+        String arrivalTime = "2022-04-02 09:30";
+        String carrier = "Air";
+
+        Flight flight = new Flight(1, from, to, carrier, departureTime, arrivalTime);
+
+        Flight savedFlight = adminController.addFlight(flight);
+
+        assertEquals(savedFlight.getFrom(), from);
+        assertEquals(savedFlight.getTo(), to);
+        assertEquals(savedFlight.getCarrier(), carrier);
+        assertEquals(savedFlight.getDepartureTime(), LocalDateTime.of(2022, 4, 2, 8, 30));
+        assertEquals(savedFlight.getArrivalTime(), LocalDateTime.of(2022, 4, 2, 9, 30));
+
+    }
+}
